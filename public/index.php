4cf2d320963765f357d743ba0cb966f88b3a33b9
Christian Varela <dobleclick.mx@gmail.com>
2020-01-23 20:07:32 -0500

    Application implementation

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php 
+include './src/StringTransformation.php';
+$stringTransformation = new conquerorsoft\coception\StringTransformation();
+
+?>
+<h1>String transformation</h1>
+<form id="encode_form" method="post">
+    <label>Plain string:</label>
+    <input type="text" id="plain_string_input" name="plain_string_input"/>
+    <input type="submit" id="encode_button" name="encode_button" value="Encode"/>
+</form>
+
+<form id="decode_form" method="post">
+    <label>Encoded string:</label>
+    <input type="text" id="encoded_string_input" name="encoded_string_input"/>
+    <input type="submit" id="decode_button" name="decode_button" value="Decode"/>
+</form>
+
+<?php
+if(isset($_POST['encode_button'])) {
+    $plainStringInput = filter_input(
+            INPUT_POST,
+            'plain_string_input',
+            FILTER_SANITIZE_SPECIAL_CHARS);
+    $encodedString = htmlspecialchars($stringTransformation->encode($plainStringInput));
+    echo "'$plainStringInput' plain string is encoded as '$encodedString'";
+}
+
+if(isset($_POST['decode_button'])) {
+    $encodedString = filter_input(
+            INPUT_POST,
+            'encoded_string_input',
+            FILTER_SANITIZE_SPECIAL_CHARS);
+    $plainStringInput = htmlspecialchars($stringTransformation->decode($encodedString));
+    echo "'$encodedString' encoded string is decoded as '$plainStringInput'";
+}
---
+++ src/StringTransformation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace conquerorsoft\coception;
+
+class StringTransformation
+{
+    protected const ALPHA_DECODED = "abcdefghijklmnopqrstuvwxyz ";
+    protected const ENCODED = ['å','Å','ç','ı','´','ƒ','©','˙','ˆ','Ç','˚','¬','µ','˜','ø','π','œ','®','ß','Î','¨','¡','Ï','Ó','¥','Ω','  '];
+    
+    public function encode(string $string): string
+    {
+        $string_to_return = "";
+        for ($i = 0; $i < strlen($string); $i++) {
+            $pos = strpos(self::ALPHA_DECODED, $string[$i]);
+            $string_to_return .= self::ENCODED[$pos];
+        }
+        return $string_to_return;
+    }
+    
+    public function decode(string $string): string
+    {
+        $string_to_return = "";
+        for ($i = 0; $i < strlen($string); $i += 2) {
+            $pos = array_search(substr($string, $i, 2), self::ENCODED);
+            $string_to_return .= self::ALPHA_DECODED[$pos];
+        }
+        return $string_to_return;
+    }
+}
